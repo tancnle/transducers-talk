49eed14bd5659b2302331ffbb55e4af910a71678
Tan Le <tanle.oz@gmail.com>
2015-03-28 10:52:15 +1100

    Copy transduce-stream file for live coding

--- transduce-stream.src.js
+++ transduce-stream.src.js
@@ -4,11 +4,32 @@ var stream = require('transduce-stream');
 
 var argv = require('optimist')
     .usage('Usage: $0 -length [num] -sub [string] -method [file|stdin]')
-    // .demand(['length', 'sub', 'method'])
+    .demand(['length', 'sub', 'method'])
     .argv;
 
+var withLengthOf = function(length) {
+  return function(str) {
+    return str.length === length;
+  };
+};
+
+var containString = function(regex) {
+  return function(str) {
+    var re = new RegExp(regex);
+    return re.test(str);
+  };
+};
+
+var withSpaces = function(str) {
+  return str + ' ';
+};
+
 var transducer = _r().lines()
+                     .filter(withLengthOf(argv.length))
+                     .filter(containString(argv.sub))
+                     .map(withSpaces)
                      .take(10)
+                     .push('\n')
                      .compose();
 
 if (argv.method === 'file') {
