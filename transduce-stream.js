a31d1ac6d0772754c154470dce7ee3f38dc7ce25
Tan Le <tanle.oz@gmail.com>
2017-10-25 00:02:07 +1100

    Fix stdin stream.

--- transduce-stream.js
+++ transduce-stream.js
@@ -7,14 +7,14 @@ const argv = require('optimist')
     .demand(['length', 'limit'])
     .argv;
 
-const transducer = _r().lines()
+const transducer = _r().words()
                    .filter(word => word.length === argv.length)
+                   .unique()
                    .take(argv.limit)
                    .map(word => word + "\n")
                    .compose();
 
 if (argv.method === 'stdin') {
-  process.stdin.resume();
   process.stdin.pipe(stream(transducer)).pipe(process.stdout);
 } else {
   const readStream = fs.createReadStream('/usr/share/dict/words');
