3711dfc910793cc3b6738b05b58a325c118528d6
Tan Le <tanle.oz@gmail.com>
2017-10-25 22:06:12 +1100

    Modernise node js code syntax.

--- transduce-stream.src.js
+++ transduce-stream.src.js
@@ -1,41 +1,38 @@
-var fs = require('fs');
-var _r = require('underarm');
-var stream = require('transduce-stream');
+const fs = require('fs');
+const _r = require('underarm');
+const stream = require('transduce-stream');
 
-var argv = require('optimist')
-    .usage('Usage: $0 -length [num] -sub [string] -method [file|stdin]')
-    .demand(['length', 'sub', 'method'])
+const argv = require('optimist')
+    .usage('Usage: $0 -length [num] -sub [string] -limit [num] -method [file|stdin]')
+    .default({limit: 10, method: 'file'})
+    .demand(['length', 'sub'])
     .argv;
 
-var withLengthOf = function(length) {
-  return function(str) {
-    return str.length === length;
-  };
+const withLengthOf = (length) => {
+  return (str) => (str.length === length);
 };
 
-var containString = function(regex) {
-  return function(str) {
-    var re = new RegExp(regex);
+const containString = (regex) => {
+  return (str) => {
+    const re = new RegExp(regex);
     return re.test(str);
   };
 };
 
-var withSpaces = function(str) {
-  return str + ' ';
+const withNewLine = (str) => {
+  return str + "\n";
 };
 
-var transducer = _r().lines()
-                     .filter(withLengthOf(argv.length))
-                     .filter(containString(argv.sub))
-                     .map(withSpaces)
-                     .take(10)
-                     .push('\n')
-                     .compose();
+const transducer = _r().words()
+                       .filter(withLengthOf(argv.length))
+                       .filter(containString(argv.sub))
+                       .take(argv.limit)
+                       .map(withNewLine)
+                       .compose();
 
 if (argv.method === 'file') {
-  var readStream = fs.createReadStream('/usr/share/dict/words');
+  const readStream = fs.createReadStream('/usr/share/dict/words');
   readStream.pipe(stream(transducer)).pipe(process.stdout);
 } else {
-  process.stdin.resume();
   process.stdin.pipe(stream(transducer)).pipe(process.stdout);
 }
