b673304dfee1513b6922af9f689419e3d11cee9b
Tan Le <tanle.oz@gmail.com>
2017-10-28 14:39:45 +1100

    Convert to modern Node-js syntax.

--- transducer-benchmark.js
+++ transducer-benchmark.js
@@ -1,34 +1,34 @@
-var t = require('transducers-js');
-var trj = require('transducers.js');
-var tr = require('transduce');
-var _r = require('underarm');
-var f = require('fkit');
+const t = require('transducers-js');
+const trj = require('transducers.js');
+const tr = require('transduce');
+const _r = require('underarm');
+const f = require('fkit');
 
-var Benchmark = require('benchmark');
-var suite = new Benchmark.Suite();
+const Benchmark = require('benchmark');
+const suite = new Benchmark.Suite();
 
-var increment = function(n) { return n + 1; };
-var isEven = function(n) { return n % 2 === 0; };
+const increment = n => n + 1;
+const isEven = n => (n % 2 === 0);
 
-suite.add('transducers-js', function() {
+suite.add('transducers-js', () => {
   t.into([], t.comp(t.map(increment), t.filter(isEven)), [0,1,2,3,4]);
 })
-.add('transducers.js', function() {
+.add('transducers.js', () => {
   trj.into([], trj.compose(trj.map(increment), trj.filter(isEven)), [0,1,2,3,4]);
 })
-.add('transduce', function() {
+.add('transduce', () => {
   tr.into([], tr.compose(tr.map(increment), tr.filter(isEven)), [0,1,2,3,4]);
 })
-.add('underarm', function() {
+.add('underarm', () => {
   _r.into([], _r.compose(_r.filter(isEven), _r.map(increment)), [0,1,2,3,4]);
 })
-.add('fkit', function() {
+.add('fkit', () => {
   f.compose(f.filter(isEven), f.map(increment))([0,1,2,3,4]);
 })
-.on('cycle', function(event) {
+.on('cycle', (event) => {
   console.log(String(event.target));
 })
-.on('complete', function() {
-  console.log('Fastest is ' + this.filter('fastest').map('name'));
+.on('complete', () => {
+  console.log('Fastest is ' + suite.filter('fastest').map('name'));
 })
 .run({ 'async': true });
