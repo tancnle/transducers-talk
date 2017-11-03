b0999c9bcc730a0a320d8e5dc5d1e274c30180da
Tan Le <tanle.oz@gmail.com>
2017-11-03 14:29:44 +1100

    Add lodash for becnhmarking and format code with prettier-js

--- transducer-benchmark.js
+++ transducer-benchmark.js
@@ -1,34 +1,40 @@
-const t = require('transducers-js');
-const trj = require('transducers.js');
-const tr = require('transduce');
-const _r = require('underarm');
-const f = require('fkit');
+const t = require("transducers-js");
+const trj = require("transducers.js");
+const tr = require("transduce");
+const _r = require("underarm");
+const _ = require("lodash");
+const f = require("fkit");
 
-const Benchmark = require('benchmark');
+const Benchmark = require("benchmark");
 const suite = new Benchmark.Suite();
 
+const numbers = [0, 1, 2, 3, 4];
 const increment = n => n + 1;
-const isEven = n => (n % 2 === 0);
+const isEven = n => n % 2 === 0;
 
-suite.add('transducers-js', () => {
-  t.into([], t.comp(t.map(increment), t.filter(isEven)), [0,1,2,3,4]);
-})
-.add('transducers.js', () => {
-  trj.into([], trj.compose(trj.map(increment), trj.filter(isEven)), [0,1,2,3,4]);
-})
-.add('transduce', () => {
-  tr.into([], tr.compose(tr.map(increment), tr.filter(isEven)), [0,1,2,3,4]);
-})
-.add('underarm', () => {
-  _r.into([], _r.compose(_r.filter(isEven), _r.map(increment)), [0,1,2,3,4]);
-})
-.add('fkit', () => {
-  f.compose(f.filter(isEven), f.map(increment))([0,1,2,3,4]);
-})
-.on('cycle', (event) => {
-  console.log(String(event.target));
-})
-.on('complete', () => {
-  console.log('Fastest is ' + suite.filter('fastest').map('name'));
-})
-.run({ 'async': true });
+suite
+  .add("transducers-js", () => {
+    t.into([], t.comp(t.map(increment), t.filter(isEven)), numbers);
+  })
+  .add("transducers.js", () => {
+    trj.into([], trj.compose(trj.map(increment), trj.filter(isEven)), numbers);
+  })
+  .add("transduce", () => {
+    tr.into([], tr.compose(tr.map(increment), tr.filter(isEven)), numbers);
+  })
+  .add("underarm", () => {
+    _r.into([], _r.compose(_r.filter(isEven), _r.map(increment)), numbers);
+  })
+  .add("fkit", () => {
+    f.compose(f.filter(isEven), f.map(increment))(numbers);
+  })
+  .add("lodash", () => {
+    _.chain(numbers).filter(isEven).map(increment).value();
+  })
+  .on("cycle", event => {
+    console.log(String(event.target));
+  })
+  .on("complete", () => {
+    console.log("Fastest is " + suite.filter("fastest").map("name"));
+  })
+  .run({ async: true });
